ad379e7aea588359c9a35d4e0fdc741454e57ba9
johnson <xubin990510@126.com>
2016-01-16 13:06:45 +0800

    fixed indent

--- src/link.cpp
+++ src/link.cpp
@@ -15,11 +15,11 @@ DWORD WINAPI Recv(LPVOID lp)
 	char Buf[1001];
 	while (1)
 	{
-	    if (recv(sockClient, Buf, 100, 0) == -1)
+		if (recv(sockClient, Buf, 100, 0) == -1)
 			break;									//接收客户端数据
 		int a = Buf[0];
 		SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY|a);
-	    cout << Buf + 1 << endl;
+		cout << Buf + 1 << endl;
 		SetConsoleTextAttribute(handle, 7);
 	}
 }
@@ -28,13 +28,13 @@ DWORD WINAPI Recv(LPVOID lp)
 int main()
 {
 	SOCKADDR_IN addrSrv;
-    WSADATA wsd;
-    char recvBuf[100];
-    if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
-    {
-        printf("找不到连接\n");
-        return 0;
-    }
+	WSADATA wsd;
+	char recvBuf[100];
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
+	{
+		printf("找不到连接\n");
+		return 0;
+	}
 	addrSrv.sin_addr.S_un.S_addr = *(u_long *) gethostbyname("beijingsunsiyu.oicp.net") -> h_addr_list[0];//inet_addr("103.44.145.243");
 	printf("%s\n", inet_ntoa(addrSrv.sin_addr)); // 转换为字符串输出
 	addrSrv.sin_family = AF_INET;
@@ -53,13 +53,13 @@ int main()
 	cin >> nickname;
 	send(sockClient, nickname, strlen(nickname) + 1, 0);
 	system("cls");
-    thread = CreateThread(NULL, 0, Recv, NULL, NULL, NULL);
+	thread = CreateThread(NULL, 0, Recv, NULL, NULL, NULL);
 	char a[1001];
-    while (1)
-    {
-    	cin >> a;
+	while (1)
+	{
+		cin >> a;
 		send(sockClient, a, strlen(a) + 1, 0);
 	}
-    closesocket(sockClient);//关闭连接
-    WSACleanup();
+	closesocket(sockClient);//关闭连接
+	WSACleanup();
 }
